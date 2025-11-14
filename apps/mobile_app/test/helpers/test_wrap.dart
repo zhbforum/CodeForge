2a1eb0940ecb31431b8919214b00db3c5b8f5373
zhbforum <lyosha.minakoff@gmail.com>
2025-11-14 23:44:08 +0100

    test: allow provider overrides in wrap helper

--- apps/mobile_app/test/helpers/test_wrap.dart
+++ apps/mobile_app/test/helpers/test_wrap.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-Widget wrap(Widget child) {
+Widget wrap(Widget child, {List<Override> overrides = const []}) {
   return ProviderScope(
+    overrides: overrides,
     child: MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(body: child),
